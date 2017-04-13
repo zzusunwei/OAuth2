14a880c83ef10ee29cc43925194681ce62ccf7ac
Burgess_sun <zzusunwei@126.com>
2017-04-13 09:01:06 +0800

    init

---
+++ authorServer/src/main/java/com/datou/author/AuthorServerApplication.java
@@ -0,0 +1,11 @@
+package com.datou.author;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorServerApplication{
+    public static void main(String[] args) {
+		SpringApplication.run(AuthorServerApplication.class, args);
+	}
+}
